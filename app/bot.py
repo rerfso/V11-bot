418525c1407d5c4529d59ea33a00ed5eb171af25
rerfso <xamlotwitch@gmail.com>
2026-06-27 16:43:46 +0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -3,16 +3,31 @@ import requests
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-API = os.getenv("API_URL", "http://127.0.0.1:8000/chat")
+API = os.getenv("API_URL")
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    r = requests.post(API, json={
-        "text": update.message.text,
-        "user_id": update.message.from_user.id
-    }).json()
+    try:
+        r = requests.post(
+            API,
+            json={
+                "text": update.message.text,
+                "user_id": update.message.from_user.id,
+            },
+            timeout=10,
+        )
 
-    await update.message.reply_text(r["response"])
+        print("STATUS:", r.status_code)
+        print("BODY:", r.text)
+
+        data = r.json()
+
+        await update.message.reply_text(
+            data.get("response", f"Ошибка API:\n{data}")
+        )
+
+    except Exception as e:
+        await update.message.reply_text(f"Ошибка:\n{e}")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT, handler))
